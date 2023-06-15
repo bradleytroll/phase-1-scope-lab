822c489ad8419fae52aa1aa49ebbdeec4a8dca65
Bradley Troll <bradleytroll@gmail.com>
2023-06-15 10:26:25 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
-// Write your solution in this file!
+//Redoing old labs for extra practice. New code below; original code commented out at bottom.
+
 var customerName = 'bob';
+
 function upperCaseCustomerName() {
     customerName = customerName.toUpperCase();
-    return customerName
+    return customerName;
 };
+
 function setBestCustomer() {
     bestCustomer = 'not bob';
     return bestCustomer;
 };
+
 function overwriteBestCustomer() {
-    bestCustomer = 'maybe bob'
-    return bestCustomer;
-}
-const leastFavoriteCustomer = 'marvin';
-function changeLeastFavoriteCustomer() {
-    leastFavoriteCustomer = 'karen';
-    return leastFavoriteCustomer;
+    bestCustomer = 'maybe bob';
 };
+
+const leastFavoriteCustomer = 'karen'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'chad';
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Write your solution in this file!
+// var customerName = 'bob';
+// function upperCaseCustomerName() {
+//     customerName = customerName.toUpperCase();
+//     return customerName
+// };
+// function setBestCustomer() {
+//     bestCustomer = 'not bob';
+//     return bestCustomer;
+// };
+// function overwriteBestCustomer() {
+//     bestCustomer = 'maybe bob'
+//     return bestCustomer;
+// }
+// const leastFavoriteCustomer = 'marvin';
+// function changeLeastFavoriteCustomer() {
+//     leastFavoriteCustomer = 'karen';
+//     return leastFavoriteCustomer;
+// };
